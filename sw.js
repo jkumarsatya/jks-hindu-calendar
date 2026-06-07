27a687b5e511afb8f6cdc7010933d8a0327c87e7
jkumarsatya <jyotikumar008@gmail.com>
2026-06-07 14:49:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,37 @@
-self.addEventListener("install",event=>{
-self.skipWaiting();
+const CACHE_NAME = "jks-calendar-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./panchang.json",
+  "./festivals.json",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
 });
