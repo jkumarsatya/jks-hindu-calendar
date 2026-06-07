f25872c3a0b5a0724fd405ff1de14a14cc15160e
jkumarsatya <jyotikumar008@gmail.com>
2026-06-07 15:13:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
   self.clients.claim();
 });
