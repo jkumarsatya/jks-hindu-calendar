3fa4cb93b1f3422a0c306fc572d0994eda5e2a13
jkumarsatya <jyotikumar008@gmail.com>
2026-06-07 11:16:46 +0530

    Implement calendar days rendering in app.js
    Add calendar days to the calendar element in app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("JKS JavaScript Loaded");
+const calendar = document.querySelector(".calendar");
+
+calendar.innerHTML = "";
+
+for (let i = 1; i <= 30; i++) {
+  const day = document.createElement("div");
+
+  day.className = "day";
+
+  day.innerHTML = `
+    <div class="date">${i}</div>
+    <div class="tithi">शु. तिथि</div>
+    <div class="monthname">आषाढ़</div>
+  `;
+
+  calendar.appendChild(day);
+}
