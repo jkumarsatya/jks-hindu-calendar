7cfdd20e9b62ceaa58869030d2c9554e8c480c11
jkumarsatya <jyotikumar008@gmail.com>
2026-06-07 11:11:24 +0530

    Remove calendar generation and add alert

--- App.js
+++ App.js
@@ -1,16 +1,1 @@
-const calendar = document.querySelector(".calendar");
-
-calendar.innerHTML = "";
-
-for (let i = 1; i <= 30; i++) {
-  const day = document.createElement("div");
-  day.className = "day";
-
-  day.innerHTML = `
-    <div class="date">${i}</div>
-    <div class="tithi">शु. तिथि</div>
-    <div class="monthname">आषाढ़</div>
-  `;
-
-  calendar.appendChild(day);
-}
+alert("JKS JavaScript Loaded");
